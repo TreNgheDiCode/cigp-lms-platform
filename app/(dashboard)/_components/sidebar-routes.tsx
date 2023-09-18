ac71c33fd390a45a8399ebd37d3ee13894716a9c
TreNgheDiCode <gabayan170@gmail.com>
2023-09-19 04:17:36 +0700

    Layout

---
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Compass, Layout } from "lucide-react";
+import SidebarItem from "./sidebar-item";
+
+const guestRoutes = [
+  {
+    icon: Layout,
+    label: "Tổng quan",
+    href: "/",
+  },
+  {
+    icon: Compass,
+    label: "Tìm kiếm",
+    href: "/search",
+  },
+];
+
+const SidebarRoutes = () => {
+  const routes = guestRoutes;
+  return (
+    <div className="flex flex-col w-full">
+      {routes.map((route) => (
+        <SidebarItem
+          key={route.href}
+          icon={route.icon}
+          label={route.label}
+          href={route.href}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default SidebarRoutes;
