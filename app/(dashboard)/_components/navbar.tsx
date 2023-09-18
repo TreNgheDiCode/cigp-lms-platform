8e1c266ea2aa46bcfcd302e64a50ddf1573baa0b
TreNgheDiCode <gabayan170@gmail.com>
2023-09-19 04:46:59 +0700

    Dynamic Layout

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,7 +1,18 @@
+"use client"
+
 import { NavbarRoutes } from "@/components/navbar-routes";
 import { MobileSidebar } from "./mobile-sidebar";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const [mounted, isMounted] = useState(false);
+
+  useEffect(() => {
+    isMounted(true)
+  }, [])
+  
+  if(!mounted) return null;
+
   return (
     <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
       <MobileSidebar />
--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 import Logo from "./logo";
 import SidebarRoutes from "./sidebar-routes";
 
 const Sidebar = () => {
+  const [mounted, isMounted] = useState(false);
+
+  useEffect(() => {
+    isMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
     <div className="border-r h-full flex flex-col overflow-y-auto bg-white shadow-sm">
       <div className="p-6">
