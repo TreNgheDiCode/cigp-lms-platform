4b13e6f73626053c5d4d842231f4438b642ce239
TreNgheDiCode <gabayan170@gmail.com>
2023-09-20 01:25:11 +0700

    Course Creation UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { viVN } from "@clerk/localizations";
 
+import ToastProvider from "@/components/providers/toaster-provider";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,7 +21,10 @@ export default function RootLayout({
   return (
     <ClerkProvider localization={viVN}>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ToastProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
