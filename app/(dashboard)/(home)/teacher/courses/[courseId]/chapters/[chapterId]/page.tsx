00c9c4722d73ff4df250da89de7ba1f0d1a1dc2e
TreNgheDiCode <gabayan170@gmail.com>
2023-09-24 06:15:26 +0700

    Chapter Publish Action

--- app/(dashboard)/(home)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(home)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -8,6 +8,8 @@ import ChapterTitleForm from "./_components/chapter-title-form";
 import ChapterDescriptionForm from "./_components/chapter-description-form";
 import ChapterAccessForm from "./_components/chapter-access-form";
 import ChapterVideoForm from "./_components/chapter-video-form";
+import Banner from "@/components/banner";
+import { ChapterActions } from "./_components/chapter-actions";
 
 const ChapterIdPage = async ({
   params,
@@ -41,70 +43,86 @@ const ChapterIdPage = async ({
 
   const completionText = `(${completedFields}/${totalFields})`;
 
+  const isComplete = requiredFields.every(Boolean);
+
   return (
-    <div className="p-6">
-      <div className="flex items-center justify-between">
-        <div className="w-full">
-          <Link
-            href={`/teacher/courses/${params.courseId}`}
-            className="flex items-center text-sm hover:opacity-75 transition mb-6"
-          >
-            <ArrowLeft className="h-4 w-4 mr-2" />
-            Quay về tùy chỉnh khóa học
-          </Link>
-          <div className="flex items-center justify-between w-full">
-            <div className="flex flex-col gap-y-2">
-              <h1 className="text-2xl font-medium">Tùy chỉnh bài giảng</h1>
-              <span className="text-sm text-slate-700">
-                Hoàn thành đầy đủ các thông tin {completionText}
-              </span>
+    <>
+      {!chapter.isPublished && (
+        <Banner
+          variant={"warning"}
+          label="Bài giảng chưa được công khai. Bài giảng này sẽ không hiển thị trong khóa học."
+        />
+      )}
+      <div className="p-6">
+        <div className="flex items-center justify-between">
+          <div className="w-full">
+            <Link
+              href={`/teacher/courses/${params.courseId}`}
+              className="flex items-center text-sm hover:opacity-75 transition mb-6"
+            >
+              <ArrowLeft className="h-4 w-4 mr-2" />
+              Quay về tùy chỉnh khóa học
+            </Link>
+            <div className="flex items-center justify-between w-full">
+              <div className="flex flex-col gap-y-2">
+                <h1 className="text-2xl font-medium">Tùy chỉnh bài giảng</h1>
+                <span className="text-sm text-slate-700">
+                  Hoàn thành đầy đủ các thông tin {completionText}
+                </span>
+              </div>
+              <ChapterActions
+                disabled={!isComplete}
+                courseId={params.courseId}
+                chapterId={params.chapterId}
+                isPublished={chapter.isPublished}
+              />
             </div>
           </div>
         </div>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
-        <div className="space-y-4">
-          <div>
-            <div className="flex items-center gap-x-2">
-              <IconBadge icon={LayoutDashboard} />
-              <h2 className="text-xl">Cập nhật thông tin</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+          <div className="space-y-4">
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={LayoutDashboard} />
+                <h2 className="text-xl">Cập nhật thông tin</h2>
+              </div>
+              <ChapterTitleForm
+                title={chapter.title}
+                courseId={params.courseId}
+                chapterId={params.chapterId}
+              />
+              <ChapterDescriptionForm
+                description={chapter.description!}
+                courseId={params.courseId}
+                chapterId={params.chapterId}
+              />
             </div>
-            <ChapterTitleForm
-              title={chapter.title}
-              courseId={params.courseId}
-              chapterId={params.chapterId}
-            />
-            <ChapterDescriptionForm
-              description={chapter.description!}
-              courseId={params.courseId}
-              chapterId={params.chapterId}
-            />
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={Eye} />
+                <h2 className="text-xl">Quản lý truy cập</h2>
+              </div>
+              <ChapterAccessForm
+                initialData={chapter}
+                chapterId={params.chapterId}
+                courseId={params.courseId}
+              />
+            </div>
           </div>
           <div>
             <div className="flex items-center gap-x-2">
-              <IconBadge icon={Eye} />
-              <h2 className="text-xl">Quản lý truy cập</h2>
+              <IconBadge icon={Video} />
+              <h2 className="text-xl">Thêm video</h2>
             </div>
-            <ChapterAccessForm
+            <ChapterVideoForm
               initialData={chapter}
               chapterId={params.chapterId}
               courseId={params.courseId}
             />
           </div>
         </div>
-        <div>
-          <div className="flex items-center gap-x-2">
-            <IconBadge icon={Video} />
-            <h2 className="text-xl">Thêm video</h2>
-          </div>
-          <ChapterVideoForm
-            initialData={chapter}
-            chapterId={params.chapterId}
-            courseId={params.courseId}
-          />
-        </div>
       </div>
-    </div>
+    </>
   );
 };
 
